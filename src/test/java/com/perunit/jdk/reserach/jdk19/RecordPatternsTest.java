3bc20adc44747365e184fb9f42c910b59e1d29cf
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2023-04-23 20:55:07 +0200

    improved RecordPatternsTest

--- src/test/java/com/perunit/jdk/reserach/jdk19/RecordPatternsTest.java
+++ src/test/java/com/perunit/jdk/reserach/jdk19/RecordPatternsTest.java
@@ -3,25 +3,94 @@ package com.perunit.jdk.reserach.jdk19;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.perunit.jdk.reserach.jdk16.Point;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RecordPatternsTest {
 
-    private static final Point point = new Point(12, 13);
+    Object object = new Location("Home", new GPSPoint(1.0, 2.0));
 
-    private Object obj;
-
-    @BeforeEach
-    void setUp() {
-        obj = point;
+    @Test
+    void givenObject_whenTestInstanceOfAndCastIdiom_shouldMatchNewInstanceOf() {
+        // old Code
+        if (object instanceof Location) {
+            Location l = (Location) object;
+            assertThat(l).isInstanceOf(Location.class);
+        }
+        // new code
+        if (object instanceof Location l) {
+            assertThat(l).isInstanceOf(Location.class);
+        }
     }
 
     @Test
-    void testRecordPatterns() {
-        if (obj instanceof Point(int x, int y)) {
-            assertThat(x + y).isEqualTo(25);
+    void givenObject_whenTestDestruct_shouldMatch() {
+        // when
+        if (object instanceof Location(var name, var gpsPoint)) {
+            // then
+            assertThat(name).isEqualTo("Home");
+            assertThat(gpsPoint).isInstanceOf(GPSPoint.class);
         }
+
+        if (object instanceof Location(var name, GPSPoint(var lat, var lng))) {
+            assertThat(lat).isEqualTo(1.0);
+            assertThat(lng).isEqualTo(2.0);
+        }
+    }
+
+    @Test
+    void givenObjectIsNull_whenTestNullCheck_shouldNotMatch() {
+        Location l = null;
+        if (l instanceof Location location) {
+            assertThat(location).isNotNull();
+        }
+    }
+
+
+    @Test
+    void givenObject_whenTestGenericTypeInstanceOf_shouldMatch() {
+        LocationWrapper<Location> locationWrapper = new LocationWrapper<>(new Location("Home", new GPSPoint(1.0, 2.0)), "Description");
+        if (locationWrapper instanceof LocationWrapper<Location>(var ignored, var description)) {
+            assertThat(description).isEqualTo("Description");
+        }
+    }
+
+
+    @Test
+    void givenObject_whenTestSwitchExpressionWithTypePattern_shouldMatch() {
+        String result = switch (object) {
+            case Location l -> l.name();
+            default -> "default";
+        };
+        assertThat(result).isEqualTo("Home");
+        Double result2 = switch (object) {
+            case Location(var name, GPSPoint(var lat, var lng)) -> lat;
+            default -> 0.0;
+        };
+        assertThat(result2).isEqualTo(1.0);
+    }
+
+    @Test
+    void givenObject_whenTestGuardedSwitchExpressionWithTypePattern_shouldMatchAndGuard() {
+        String result = switch (object) {
+            case Location(var name, var ignored) when name.equals("Home") -> "Test";
+            case Location(var name, var ignored) -> name;
+            default -> "default";
+        };
+        assertThat(result).isEqualTo("Test");
+
+        String otherResult = switch (new Location("Other", new GPSPoint(1.0, 2.0))) {
+            case Location(var name, var ignored) when name.equals("Home") -> "Test";
+            case Location(var name, var ignored) -> name;
+            default -> "default";
+        };
+        assertThat(otherResult).isEqualTo("Other");
+
+        Object noLocation = new GPSPoint(1.0, 2.0);
+        String noLocationResult = switch (noLocation) {
+            case Location(var name, var ignored) when name.equals("Home") -> "Test";
+            case Location(var name, var ignored) -> name;
+            default -> "default";
+        };
+        assertThat(noLocationResult).isEqualTo("default");
     }
 }

0b13cdbba7e37812faadeb5a5a6f5687aa19d210
Piotr Szerszeń <SzerPMSC@qiagen.onmicrosoft.com>
2023-05-07 10:42:59 +0200

    fix of code snippet

--- src/main/java/com/perunit/jdk/reserach/jdk18/JavadocSnippets.java
+++ src/main/java/com/perunit/jdk/reserach/jdk18/JavadocSnippets.java
@@ -1,6 +1,5 @@
 package com.perunit.jdk.reserach.jdk18;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,19 +7,20 @@ import java.nio.file.Path;
 public class JavadocSnippets {
 
     /**
-     * How to write a text file with Java 7:
+     * How to write a text file with Java 18:
      * <p>
      * {@snippet :
-     * try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+     * try (var writer = Files.newBufferedWriter(path)) {
      *   writer.write(text);
      * }
      *}
+     * </p>
      *
      * @param path destination
      * @param text to be written
      */
     private void method(Path path, String text) throws IOException {
-        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+        try (var writer = Files.newBufferedWriter(path)) {
             writer.write(text);
         }
     }
